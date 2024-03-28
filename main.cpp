7e12d15cc0dee40423c69b5a8109301e08fbbf07
Aditya Aparadh <aditya.aparadh.0@gmail.com>
2024-03-28 16:01:01 +0530

    Changed axis of rotation

--- main.cpp
+++ main.cpp
@@ -112,7 +112,7 @@ int main()
 
 
      glm::mat4 modelMatrix = glm::mat4(1.0f);
-     modelMatrix = glm::rotate(modelMatrix, (float)glfwGetTime(),glm::vec3(1.0f, 0.0f, 0.0f));
+     modelMatrix = glm::rotate(modelMatrix, (float)glfwGetTime(),glm::vec3(1.0f, 1.0f, 0.0f));
 
 
         s.useUniformMatrix4fv("model",1, glm::value_ptr(modelMatrix));
